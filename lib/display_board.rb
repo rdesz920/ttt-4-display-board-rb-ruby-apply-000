7c24ed1c18af3df5d03eb713cbb9e4c70d71fe5d
Rob Desz <rob.desz@gmail.com>
2020-09-22 18:58:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,35 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board1 = [" "," "," "," "," "," "," "," "," "]
+board2 = [" "," "," "," ","X"," "," "," "," "]
+board3 = ["O"," "," "," "," "," "," "," "," "]
+board4 = ["O"," "," "," ","X"," "," "," "," "]
+board5 = ["X","X","X"," "," "," "," "," "," "]
+board6 = [" "," "," "," "," "," ","O","O","O"]
+board7 = ["X"," "," "," ","X"," "," "," ","X"]
+board8 = [" "," ","O"," ","O"," ","O"," "," "]
+board9 = ["X","O","X"," "," ","O","X","X","O"]
+
+display_board(board1)
+
+display_board(board2)
+
+display_board(board3)
+
+display_board(board4)
+
+display_board(board5)
+
+display_board(board6)
+
+display_board(board7)
+
+display_board(board8)
+
+display_board(board9)
