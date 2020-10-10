11738e4895a3f41bc836b6882dee2be8caa488e9
Rob Desz <rob.desz@gmail.com>
2020-10-10 16:56:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,31 +5,3 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-
-board1 = [" "," "," "," "," "," "," "," "," "]
-board2 = [" "," "," "," ","X"," "," "," "," "]
-board3 = ["O"," "," "," "," "," "," "," "," "]
-board4 = ["O"," "," "," ","X"," "," "," "," "]
-board5 = ["X","X","X"," "," "," "," "," "," "]
-board6 = [" "," "," "," "," "," ","O","O","O"]
-board7 = ["X"," "," "," ","X"," "," "," ","X"]
-board8 = [" "," ","O"," ","O"," ","O"," "," "]
-board9 = ["X","O","X"," "," ","O","X","X","O"]
-
-display_board(board1)
-
-display_board(board2)
-
-display_board(board3)
-
-display_board(board4)
-
-display_board(board5)
-
-display_board(board6)
-
-display_board(board7)
-
-display_board(board8)
-
-display_board(board9)
